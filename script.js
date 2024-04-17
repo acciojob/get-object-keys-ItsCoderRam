d6e20b3a03b1077759d45a00314a2d03f62701d5
ItsCoderRam <54639693+ItsCoderRam@users.noreply.github.com>
2024-04-17 18:09:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
 //your JS code here. If required.
 let student = {
-	name: "rn"
-};
-
-Object.prototype.getKeys = function() {
-	return Object.keys(this);
-};
-
+	name:"rn"
+}
+Object.prototype.getKeys = function(){
+	return Object.Keys(this);
+}
 console.log(student.getKeys());
